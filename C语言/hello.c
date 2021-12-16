46f5ed285d9232e3b56982d6f89d1b8f20225c8c
yydsprom <90077712+yydsprom@users.noreply.github.com>
2021-12-16 19:20:02 +0800

    Update hello.c

--- C语言/hello.c
+++ C语言/hello.c
@@ -1,7 +1,53 @@
 #include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<string.h>
+int panduan(int n)  //返回不同数字的个数
+{
+    int ge = n % 10;
+    int shi = n % 100 / 10;
+    int bai = n % 1000 / 100;
+    int qian = n / 1000;
+
+    int a[10] = { 0 };
+    int max = 0;
+    a[ge]++;
+    a[shi]++;
+    a[bai]++;
+    a[qian]++;
+    for (int i = 0; i < 10; i++)
+    {
+        if (max < a[i])
+            max = a[i];
+
+    }
+    if (max == 1)
+        return 4;
+    if (max == 2)
+        return 3;
+    if (max == 3)
+        return 2;
+    if (max == 4)
+        return 0;
+
+}
+
+
 
 int main()
 {
-    printf("hello");
-    return 0;
+    int year, different;
+    scanf_s("%d%d", &year, &different);
+    int i;
+    int temp;
+    int answers;
+    for (i = 0;; i++)
+    {
+        temp = year + i;
+        if (panduan(temp) == different)
+            answers = i;
+
+    }
+
+    printf("%d %d", i, year + i);
 }
